e978e06ee3fdb5d258d2d4fd7514e32f60ed4aa1
ranime-sb <ranime.sboui@esprit.tn>
2025-10-27 18:01:52 +0100

    Déploiement Render

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py migrate
-python manage.py createsuperuser --noinput || true
+python manage.py migrate --plan 
+python manage.py migrate Pixelette 0011_alter_interaction_options_and_more --fake 
+python manage.py migrate python manage.py createsuperuser --noinput || true
+
+
